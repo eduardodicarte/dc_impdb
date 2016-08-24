6318f67dacaf914d1bc094251d87b67b76d013f7
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-08-24 01:34:05 -0300

    Alteracao nos parametros unless e command para execucao do script de geracao de tabelas no banco de dados

--- manifests/postgresql/dump/tables.pp
+++ manifests/postgresql/dump/tables.pp
@@ -1,18 +1,20 @@
 class dc_impdb::postgresql::dump::tables {
+  
   file { '/tmp/generate_tables.sql':
-    ensure => file,
-    owner  => 'postgres',
-    group  => 'root',
-    mode   => '0770',
-    source => 'puppet:///modules/dc_impdb/generate_tables.sql',
+    ensure  => file,
+    owner   => 'postgres',
+    group   => 'root',
+    mode    => '0770',
+    source  => 'puppet:///modules/dc_impdb/generate_tables.sql',
+    require => Postgresql::Server::Schema[$dc_impdb::namesch]
   }
   
   exec {"generate tables":
-    command => 'psql -d $dc_impdb::namedb < generate_tables.sql',
-    cwd     => 'tmp',
+    command  => "psql -d $dc_impdb::namedb < generate_tables.sql",
+    cwd     => '/tmp',
     path    => '/usr/bin',
     user    => 'postgres',    
-    unless  => 'psql -d $dc_impdb::namedb',
+    unless => "psql -d $dc_impdb::namedb -c 'select * from dict'",
     require => File['/tmp/generate_tables.sql']
   }  
 }
