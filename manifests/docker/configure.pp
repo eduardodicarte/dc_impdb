a359588a83f2b08dad1bddff1509ce01d4e301e9
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-09-04 16:18:33 -0300

    Configuração do container docker via Dockerfile

--- manifests/docker/configure.pp
+++ manifests/docker/configure.pp
@@ -1,13 +1,24 @@
 class dc_impdb::docker::configure {
-  docker::image {'centos':
-    #tag         => '7',
-    docker_file => '/tmp/Dockerfile',
+  docker::image {'impdb':
+    image_tag   => '1',
+    docker_dir  => '/tmp/',
     subscribe   => File['/tmp/Dockerfile'],
     require     => Service['docker']
   }
   
   file { '/tmp/Dockerfile':
     ensure => file,
-    source => 'puppet:///modules/dc_impdb/Dockerfile'
+    source => 'puppet:///modules/dc_impdb/Dockerfile',
+    require => [File['/tmp/Puppetfile'],File['/tmp/site.pp']]
+  }
+  
+  file { '/tmp/Puppetfile':
+    ensure => file,
+    source => 'puppet:///modules/dc_impdb/Puppetfile'
+  }
+  
+  file{'/tmp/site.pp':
+    ensure => file,
+    source => 'puppet:///modules/dc_impdb/site.pp'
   }
 }
