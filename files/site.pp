b327da65b87a4f244713dcf14d62d491b0a46c1a
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-09-04 16:10:14 -0300

    Geração de arquivos necessários para geração e configuração automatizada de um container docker para instalação e configuração do banco de dados

---
+++ files/site.pp
@@ -0,0 +1,8 @@
+class { 'dc_impdb':
+    userdb       => 'impdb',
+    passdb       => 'imprt',
+    namedb       => 'AvdWe&sd#',
+    namesch      => 'dict',
+    nametbl      => 'el',
+    sourceModule => 'docker' 
+}
