3b05590bf03642af9b9498992a9f9da75698bf80
Eduardo Dicarte <eduardo_dicarte@yahoo.com.br>
2016-09-08 17:17:11 -0300

    Criação dos métodos necessários para criação e manutenção dos containers docker

--- manifests/docker/containers/run.pp
+++ manifests/docker/containers/run.pp
@@ -1,10 +1,54 @@
 class dc_impdb::docker::containers::run {
-  docker::run { 'run_impdb':
-    image           => 'impdb',
-    ports           => ['5432'],
-    expose          => ['2343'],
-    dns             => ['8.8.8.8','8.8.4.4'],
-    restart_service => true,
-    before_stop     => 'echo "So long, and Thanks for All the Fish"'
+
+  $name_container      = 'impdb'
+  $porta_imagem        = 80
+  $porta_container     = 8080
+  $nome_imagem         = 'impdb'
+  $recreate_container  = true
+  $stop_container      = false
+  $start_container     = false
+  $restart_container   = false
+  $create_container    = false
+
+  $dctitle = "${name_container}_${porta_container}"
+
+  if $restart_container == true or $stop_container == true or $recreate_container == true{
+    exec { "kill_container $dctitle":
+    command => "docker kill $dctitle",
+    user    => 'root',
+    group   => 'root',
+    path    => '/usr/bin/',
+    onlyif  => "docker ps | grep $dctitle"
+  }
+}
+
+  if $recreate_container == true {
+    exec { "remove_container $dctitle":
+      command => "docker rm $dctitle",
+      user    => 'root',
+      group   => 'root',
+      path    => '/usr/bin/',
+      onlyif  => "docker ps -a | grep $dctitle"
+    }
+  }
+
+  if $start_container == true {
+    exec { "start_container $dctitle":
+      command => "docker start $dctitle",
+      user    => 'root',
+      group   => 'root',
+      path    => '/usr/bin/',
+      unless  => "docker ps | grep $dctitle"
+    }
+  }
+
+if $recreate_container == true or $create_container == true {
+    exec { "$dctitle":
+      command => "docker run -d --name $dctitle -it -p $porta_container:$porta_imagem $nome_imagem",
+      user    => 'root',
+      group   => 'root',
+      path    => '/usr/bin/',
+      unless  => "docker ps -a | grep $dctitle"
+    }
   }
 }
